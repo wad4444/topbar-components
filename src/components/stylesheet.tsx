35fed14430bc4e4687dc5c58a16909d0b7ab26d7
wad4444 <77159113+wad4444@users.noreply.github.com>
2024-12-02 21:10:16 +0300

    fix: deeply copy when merging objects, add a continue statement

--- src/components/stylesheet.tsx
+++ src/components/stylesheet.tsx
@@ -2,23 +2,12 @@ import React from "@rbxts/react";
 import { StylesheetContext } from "../context";
 import { DefaultStylesheet, Stylesheet as StylesheetType } from "../style";
 import { DeepPartial } from "../utilities/types";
+import { mergeNested } from "../utilities/merge";
 
 interface Props extends React.PropsWithChildren {
 	Stylesheet: PartialStylesheet;
 }
 type PartialStylesheet = DeepPartial<StylesheetType>;
-
-function mergeNested(base: object, from: object) {
-	const cloned = table.clone(base);
-	for (const [key, value] of pairs(from)) {
-		if (typeIs(value, "table")) {
-			cloned[key as never] = mergeNested(base[key as never], value) as never;
-		}
-		cloned[key as never] = value as never;
-	}
-
-	return cloned;
-}
 
 export function Stylesheet({ Stylesheet, children }: Props) {
 	return (
