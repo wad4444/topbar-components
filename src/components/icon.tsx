736b549c8f6e6dc37ad7db56f95aa6faf7e2e9d4
wad4444 <77159113+wad4444@users.noreply.github.com>
2024-12-09 23:24:41 +0300

    feat: force height prop

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -118,8 +118,9 @@ export function Icon({
 		setTextBounds(TextService.GetTextBoundsAsync(params));
 	}, [currentText, stylesheet]);
 
-	const ICON_DIFF_Y = style === "New" && location.Type !== "Dropdown" ? 12 : 4;
-	const ICON_HEIGHT = inset.Height - ICON_DIFF_Y;
+	const ICON_DIFF_Y = style === "New" ? 12 : 4;
+	const FORCE_HEIGHT = location.Type === "Dropdown" ? stylesheet.Dropdown.ForceHeight : undefined;
+	const ICON_HEIGHT = FORCE_HEIGHT ?? inset.Height - ICON_DIFF_Y;
 	const PADDING = style === "New" ? 6 : 3;
 	const IMAGE_SIZE = ICON_HEIGHT - PADDING * 2;
 
