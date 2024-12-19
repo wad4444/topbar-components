28a9c6ce8ddf0eca4879d84cb431cba39223be95
wad4444 <77159113+wad4444@users.noreply.github.com>
2024-12-19 18:47:00 +0300

    fix: adjust formula

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -139,8 +139,8 @@ export function Icon({
 		),
 		ICON_HEIGHT,
 	);
-	const IMAGE_POS =
-		PADDING + (IMAGE_SIZE_OFF > 0 ? -IMAGE_SIZE_OFF : IMAGE_SIZE_OFF) * 0.5;
+	const IMAGE_POS = PADDING + IMAGE_SIZE_OFF * -0.5;
+	print(PADDING, IMAGE_SIZE_OFF, IMAGE_SIZE, ICON_HEIGHT);
 
 	const textLabelPos = new UDim2(
 		0,
