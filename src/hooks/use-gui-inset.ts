1ebeea227aa7343faae9ca7a840cdcaf431f1c48
wad4444 <77159113+wad4444@users.noreply.github.com>
2025-02-07 17:33:57 +0300

    fix: gui not updating accordingly to inset

--- src/hooks/use-gui-inset.ts
+++ src/hooks/use-gui-inset.ts
@@ -1,9 +1,14 @@
-import { useEventListener } from "@rbxts/pretty-react-hooks";
+import { useEventListener, useMountEffect } from "@rbxts/pretty-react-hooks";
 import { useState } from "@rbxts/react";
 import { GuiService } from "@rbxts/services";
 
 export function useGuiInset() {
 	const [inset, setInset] = useState(GuiService.TopbarInset);
+
+	useMountEffect(() => {
+		setInset(GuiService.TopbarInset)
+	})
+
 	useEventListener(GuiService.GetPropertyChangedSignal("TopbarInset"), () =>
 		setInset(GuiService.TopbarInset),
 	);
