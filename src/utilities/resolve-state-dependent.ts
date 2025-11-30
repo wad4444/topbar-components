8f94c5cfa0ffcadda46dd3065d225b3b5a7a5b03
wad4444 <77159113+wad4444@users.noreply.github.com>
2025-11-30 18:57:30 +0300

    fix: add a safeguard for invalid state

--- src/utilities/resolve-state-dependent.ts
+++ src/utilities/resolve-state-dependent.ts
@@ -1,11 +1,16 @@
 import { t } from "@rbxts/t";
 import { IconState, StateDependent } from "../components/icon";
 
-const check: t.check<Record<IconState, any>> = t.interface({
+const state_check: t.check<IconState> = t.union(
+	t.literal("selected"),
+	t.literal("deselected"),
+);
+const object_check: t.check<Record<IconState, any>> = t.interface({
 	selected: t.any,
 	deselected: t.any,
 });
 
 export function stateful<T>(value: StateDependent<T>, state: IconState): T {
-	return check(value) ? value[state] : value;
+	assert(state_check(state), `Cannot resolve invalid state: ${state}`);
+	return object_check(value) ? value[state] : value;
 }
